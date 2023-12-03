07cc17161a943bb496280bd50aaf06b5812f9448
MrSquid0 <gonzalo0@correo.ugr.es>
2023-12-03 04:07:26 +0100

    Authentication added to the API

--- Project/e-commerce/etienda/api.py
+++ Project/e-commerce/etienda/api.py
@@ -2,13 +2,12 @@
 from ninja_extra import NinjaExtraAPI, api_controller, http_get
 from typing import List
 from ninja import NinjaAPI, Schema
-from . import Queries
+from ninja.security import django_auth, HttpBearer
 from pydantic import ValidationError, validator
 from . import mongo_operations_api
 import logging
 
 logger = logging.getLogger(__name__)
-api = NinjaExtraAPI()
 
 
 class Rate(Schema):
@@ -38,13 +37,22 @@ class ErrorSchema(Schema):
     message: str
 
 
-@api.get("/products", tags=['DAI SHOP'], response={200: List[ProductSchema]})
+class GlobalAuth(HttpBearer):
+    def authenticate(self, request, token):
+        if token == "dai":
+            return token
+
+
+api = NinjaExtraAPI(auth=GlobalAuth())
+
+
+@api.get("/products", tags=['DAI SHOP'], response={200: List[ProductSchema]}, auth=None)
 def get_list_of_products(request, start: int = 0, end: int = 10):
     products = mongo_operations_api.list_products(start, end)
     return products
 
 
-@api.get("/products/{id}", tags=['DAI SHOP'], response={200: ProductSchema, 404: ErrorSchema})
+@api.get("/products/{id}", tags=['DAI SHOP'], response={200: ProductSchema, 404: ErrorSchema}, auth=None)
 def get_product(request, id: str):
     try:
         product = mongo_operations_api.search_product(id)
@@ -56,6 +64,11 @@ def get_product(request, id: str):
 
 @api.post("/products", tags=['DAI SHOP'], response={201: ProductSchema, 400: ErrorSchema})
 def add_product(request, payload: ProductSchemaIn):
+    if not payload.title or not payload.title[0].isupper():
+        error_message = "The first letter of the product name must be uppercase."
+        logger.error(f"There was a problem trying to add the product throughout the API! {error_message}")
+        return 400, {"message": error_message}
+
     try:
         created_product = mongo_operations_api.create_product(payload)
         logger.info("A product was added throughout the API!")
@@ -65,15 +78,20 @@ def add_product(request, payload: ProductSchemaIn):
         return 400, {"message": str(e)}
 
 
-@api.put("/products/{id}", tags=['DAI SHOP'], response={202: ProductSchema, 404: ErrorSchema})
+@api.put("/products/{id}", tags=['DAI SHOP'], response={201: ProductSchema, 400: ErrorSchema})
 def modify_product(request, id: str, payload: ProductSchemaIn):
+    if not payload.title or not payload.title[0].isupper():
+        error_message = "The first letter of the product name must be uppercase."
+        logger.error(f"There was a problem trying to add the product throughout the API! {error_message}")
+        return 400, {"message": error_message}
+
     try:
-        modified_data = mongo_operations_api.modify_product(id, payload)
-        logger.info("Some product information was modified throughout the API!")
-        return 202, modified_data
-    except:
-        logger.error("There was a problem trying to modify the product throughout the API!")
-        return 404, {"message": "Product not found"}
+        data_modified = mongo_operations_api.modify_product(id, payload)
+        logger.info("A product was added throughout the API!")
+        return 201, data_modified
+    except ValidationError as e:
+        logger.error("There was a problem trying to add the product throughout the API!")
+        return 400, {"message": str(e)}
 
 
 @api.delete("/products/{id}", tags=['DAI SHOP'], response={204: None, 404: ErrorSchema})
